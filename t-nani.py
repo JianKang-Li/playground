daadddf4c3e64b1fac6abb38991724e5d27198a1
ljk <3104653373@qq.com>
2022-07-21 14:29:30 +0800

    新增网址，修改文件命名

--- t-nani.py
+++ t-nani.py
@@ -1,41 +1,61 @@
-#coding=utf-8
+# coding=utf-8
 # 导包
 # 发送请求
 import urllib
 import requests
 import re
 import os
+
 # 解析网站源码
 from bs4 import BeautifulSoup
 
+# 时间
+import time
+
+
+def getTime():
+    strs = time.localtime()
+    return (
+        str(strs.tm_year)
+        + str(strs.tm_mon)
+        + str(strs.tm_mday)
+        + str(strs.tm_hour)
+        + str(strs.tm_min)
+        + str(strs.tm_sec)
+    )
+
+
 # 爬取网址
 # url="http://www.t-nani.co.kr/index.html"
-baseUrl="http://www.t-nani.co.kr/"
+baseUrl = "http://www.t-nani.co.kr/"
 # 图片保存位置
-path = 'F:\\python\\test\\img'
+path = "F:\\python\\test\\img"
 
 # 移除js、css
 def remove_js_css(content):
-    r = re.compile(r'''<script.*?</script>''', re.I | re.M | re.S)
-    s = r.sub('', str(content))
-    r = re.compile(r'''<style.*?</style>''', re.I | re.M | re.S)
-    s = r.sub('', s)
-    r = re.compile(r'''<!--.*?-->''', re.I | re.M | re.S)
-    s = r.sub('', s)
+    r = re.compile(r"""<script.*?</script>""", re.I | re.M | re.S)
+    s = r.sub("", str(content))
+    r = re.compile(r"""<style.*?</style>""", re.I | re.M | re.S)
+    s = r.sub("", s)
+    r = re.compile(r"""<!--.*?-->""", re.I | re.M | re.S)
+    s = r.sub("", s)
     return s
+
 
 # 移除空格
 def remove_empty_line(content):
-    r = re.compile(r'''^\s+$''', re.M | re.S)
-    s = r.sub('', str(content))
-    r = re.compile(r'''\n+''', re.M | re.S)
-    s = r.sub('\n', s)
+    r = re.compile(r"""^\s+$""", re.M | re.S)
+    s = r.sub("", str(content))
+    r = re.compile(r"""\n+""", re.M | re.S)
+    s = r.sub("\n", s)
     return s
 
+
 def remove_any_tag(s):
-    s = re.sub(r'''<[^>]+>''', '', s)
+    s = re.sub(r"""<[^>]+>""", "", s)
     s.replace(" ", "")
     return s.strip()
+
 
 def getImgList(html):
     reg = r'src=".*\.jpe?g.*"'
@@ -43,29 +63,31 @@ def getImgList(html):
     imglist = re.findall(imgre, html)
     return imglist
 
+
 # 获取图片
 def getImg(imglist):
-    length=str(len(imglist))
+    times = getTime()
+    length = str(len(imglist))
     if not os.path.isdir(path):
         os.makedirs(path)
-    paths=path+'\\'
-    x=1
-    print("Start %d images!"%(int(length)))
+    paths = path + "\\"
+    x = 1
+    print("Start %d images!" % (int(length)))
     for imgurl in imglist:
-        imgurl=imgurl.replace('src="',"")
-        imgurl=imgurl.replace('"',"")
-        url=baseUrl+imgurl
-        print(url+" "+str(x)+"/"+length)
-        f = open(paths + str(x) + '.jpg', 'wb')
+        imgurl = imgurl.replace('src="', "")
+        imgurl = imgurl.replace('"', "")
+        url = baseUrl + imgurl
+        print(url + " " + str(x) + "/" + length)
+        f = open(paths + times + "_" + str(x) + ".jpg", "wb")
         f.write((urllib.request.urlopen(url)).read())
-        x=x+1
+        x = x + 1
     print("Done all!")
 
 
 # 获取html源码
 def getHtml(url):
     res = requests.get(url)
-    if (res.status_code == 200):
+    if res.status_code == 200:
         html = res.content
         soup = BeautifulSoup(html, "html.parser")
         html = remove_js_css(soup)
@@ -76,7 +98,8 @@ def getHtml(url):
         print("请求失败" + str(res.status_code))
         return -1
 
-urlList=[
+
+urlList = [
     "http://www.t-nani.co.kr/index.html",
     "http://www.t-nani.co.kr/shop/shopbrand.html?type=P&xcode=005",
     "http://www.t-nani.co.kr/shop/bestseller.html?xcode=BEST",
@@ -90,19 +113,11 @@ urlList=[
     "http://www.t-nani.co.kr/shop/shopbrand.html?type=P&xcode=034",
 ]
 
-# urlList=[]
-img=[]
+img = []
 for i in urlList:
-    html=getHtml(i)
+    html = getHtml(i)
     imglist = getImgList(html)
     img.extend(imglist)
-    img=set(img)
-    img=list(img)
+    img = set(img)
+    img = list(img)
 getImg(img)
-
-# html=getHtml(baseUrl)
-# imglist=getImgList(html)
-# print(len(imglist))
-# getImg(imglist)
-
-
--- test.py
+++
@@ -1,9 +0,0 @@
-
-import requests        #µ¼Èërequests°ü
-from bs4 import    BeautifulSoup
-url='http://www.cntour.cn/'
-strhtml=requests.get(url)
-soup=BeautifulSoup(strhtml.text,'lxml')
-data = soup.select('#main>div>div.mtop.firstMod.clearfix>div.centerBox>ul.newsList>li>a')
-print(data)
-
