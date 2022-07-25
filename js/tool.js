a25292cfee58e4db9229d6127c38db0cd6fbe2aa
ljk <3104653373@qq.com>
2022-07-25 13:42:55 +0800

    新增工具函数

--- js/tool.js
+++ js/tool.js
@@ -27,9 +27,76 @@
     return res.json()
   }
 
+  // 创建随机数字数组
+  function CNumbers(n, min, max) {
+    let a = [];
+    for (let i = 0; i < n; i++) {
+      a[i] = Math.floor(Math.random() * (max - min)) + min;
+    }
+    return a;
+  };
+
+  //创建指定长度的字符串
+  function CString(n) {
+    let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
+    for (let j = 0; j < n; j++) {
+      result += str[Math.floor(Math.random() * str.length)];
+    }
+    return result;
+  };
+
+  //创建指定范围内的随机数字
+  function CNumber(min, max) {
+    let a;
+    a = Math.floor(Math.random() * (max - min)) + min;
+    return a;
+  };
+
+  //获取当前时间并格式化为（hh:mm:ss）
+  function Ctime() {
+    let date = new Date();
+    let time =
+      date.getHours().toString().padStart(2, 0) +
+      ":" +
+      date.getMinutes().toString().padStart(2, 0) +
+      ":" +
+      date.getSeconds().toString().padStart(2, 0);
+    return time;
+  };
+
+  //深度复制
+  function dpClone(target, map = new Map()) {
+    if (target.constructor === Date) {
+      return new Date(target);
+    }
+    if (target.constructor === RegExp) {
+      return new RegExp(target);
+    }
+    if (target instanceof Error) return new Error(target.message);
+    if (target instanceof Function)
+      return function proxy(...args) {
+        return target.call(this, args);
+      };
+    if (typeof target != "object") return target;
+    if (map.has(target)) return map.get(target);
+    let newObj = new target.constructor();
+    map.set(target, newObj);
+    for (let key in target) {
+      if (target.hasOwnProperty(key)) {
+        newObj[key] = _completeDeepClone(target[key], map);
+      }
+    }
+    return newObj;
+  };
 
   let tool = {
-    http: http
+    http,
+    CNumber,
+    CNumbers,
+    CString,
+    Ctime,
+    dpClone,
   }
 
   window.tool = tool
