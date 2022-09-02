c4a0b4240abf7f25b80e60605c59d10a722be234
ljk <3104653373@qq.com>
2022-09-02 15:30:51 +0800

    Dom

--- js/tool.js
+++ js/tool.js
@@ -16,11 +16,26 @@
     constructor(dom) {
       this.dom = dom
     }
-    addClass(className) {
-      this.dom.classList.add(className)
+    addClass(clas) {
+      let classNames = clas.splice(" ")
+      classNames.forEach(item => {
+        this.dom.classList.add(item)
+      })
     }
     removeClass(className) {
       this.dom.classList.remove(className)
+    }
+
+    setText(text) {
+      this.dom.innerText = text
+    }
+
+    getText() {
+      return this.dom.innerText || this.dom.innerHTML
+    }
+
+    remove() {
+      this.dom.remove()
     }
   }
 
