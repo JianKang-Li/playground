2bbce5f69ed3606787d1cf0e565d1c9ba7449183
ljk <3104653373@qq.com>
2022-09-14 13:18:01 +0800

    大数相加

--- js/tool.js
+++ js/tool.js
@@ -550,6 +550,28 @@
     }
   }
 
+  // 大数相加
+  function addBig(a, b) {
+    //取两个数字的最大长度
+    let maxLength = Math.max(a.length, b.length);
+    //用0去补齐长度
+    a = a.padStart(maxLength, 0);
+    b = b.padStart(maxLength, 0);
+    //定义加法过程中需要用到的变量
+    let t = 0;
+    let f = 0;   //"进位"
+    let sum = "";
+    for (let i = maxLength - 1; i >= 0; i--) {
+      t = parseInt(a[i]) + parseInt(b[i]) + f;
+      f = Math.floor(t / 10);
+      sum = t % 10 + sum;
+    }
+    if (f !== 0) {
+      sum = '' + f + sum;
+    }
+    return sum;
+  }
+
   let tool = {
     unique,
     CNumber,
@@ -584,6 +606,7 @@
     Notify,
     isEqual,
     isEmpty,
+    addBig,
   }
 
   window.tool = tool
