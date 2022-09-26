366d485030e40f68c54dae37de714f04c157f05f
ljk <3104653373@qq.com>
2022-09-26 09:44:49 +0800

    Vshake组件

---
+++ js/Vue/components/Vshake.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="{ Vshake: shake }">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: ["shake"],
+};
+</script>
+
+<style>
+@keyframes shake2 {
+  10%,
+  90% {
+    transform: translateX(-1px);
+  }
+
+  20%,
+  80% {
+    transform: translateX(2px);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translateX(-4px);
+  }
+
+  40%,
+  60% {
+    transform: translateX(4px);
+  }
+}
+
+.Vshake {
+  animation: shake2 1s;
+}
+</style>
