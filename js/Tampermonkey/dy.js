a6a2d7b1f3bb586c980a3e04bcbfef122662b9f5
ljk <3104653373@qq.com>
2022-10-08 10:57:19 +0800

    修改图片获取

--- js/Tampermonkey/dy.js
+++ js/Tampermonkey/dy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         dy
 // @namespace    http://tampermonkey.net/
-// @version      0.2.1
+// @version      0.2.2
 // @description  抖音网页视频链接下载!
 // @author       jk小帅
 // @match        https://www.douyin.com/*
@@ -33,15 +33,28 @@
     const dy1 = document.querySelector('#dy1')
 
     dy1.addEventListener('click', function () {
-      let imgs = document.querySelectorAll('.swiper-slide img')
-      let set = new Set()
       const videos = document.querySelectorAll('video')
       const len = videos.length
       let video = len === 1 ? videos[0] : len === 3 ? videos[1] : videos[0]
       video.pause()
+      let imgs;
+      if (location.href.indexOf("user") !== -1) {
+        imgs = document.querySelectorAll('.swiper-container .swiper-wrapper img')
+      } else {
+        try {
+          imgs = document.querySelectorAll(".swiper-wrapper")[1].getElementsByTagName('img')
+        } catch {
+          alert('没有图片')
+          return;
+        }
+      }
+
+      let set = new Set()
+      imgs = Array.from(imgs)
       imgs.forEach((item) => {
         set.add(item.src)
       })
+      console.log(set);
       if (set.size === 0) {
         alert("获取失败")
         return;
