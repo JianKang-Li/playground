17e1a815397cc5ad4a37c3df459b91bfd0a7273a
ljk <3104653373@qq.com>
2022-10-19 18:25:20 +0800

    日期比较

--- js/module.js
+++ js/module.js
@@ -388,7 +388,15 @@ export function rem2px(rem) {
 
 // 比较两个日期之间差的天数
 export function dayDif(date1, date2) {
-  return Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000)
+  if (typeof date1 === 'number') {
+    date1 = new Date(date1)
+  }
+  if (typeof date2 === 'number') {
+    date2 = new Date(date2)
+  }
+  let start = new Date(`${date1.getFullYear()}-${date1.getMonth()}-${date1.getDate()}`)
+  let end = new Date(`${date2.getFullYear()}-${date2.getMonth()}-${date2.getDate()}`)
+  return Math.ceil(Math.abs(start.getTime() - end.getTime()) / 86400000)
 }
 
 // 事件总线
