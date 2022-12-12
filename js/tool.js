7272d2abd64e8e7e23ff5199c9ce6c7bf7d7e3ef
ljk <3104653373@qq.com>
2022-12-12 22:03:21 +0800

    jsonp

--- js/tool.js
+++ js/tool.js
@@ -434,6 +434,14 @@
       inner()
     })
   }
+
+  function jsonp(url, option) {
+    if (!url || !option) return;
+    let script = document.createElement('script');
+    url = /\?/.test(url) ? url + `&${option.name ? option.name : 'callback'}=` + option.callback : url + `?${option.name ? option.name : 'callback'}=` + option.callback
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
   //#endregion
 
   //#region 时间操作方法
@@ -1439,6 +1447,7 @@
     getQueryString,
     concurRequest,
     retry,
+    jsonp,
     //#endregion
 
     //#region 时间
