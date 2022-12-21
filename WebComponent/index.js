3d9f35635299bb91f58652d5c58f3faf9cbde560
ljk <3104653373@qq.com>
2022-12-21 16:08:53 +0800

    开关

--- WebComponent/index.js
+++ WebComponent/index.js
@@ -20,13 +20,17 @@ attributeChangedCallback
 */
 
 /* 工厂函数用于创建自定义组件，参数name:自定义组件名；html:自定义模板 */
-function createComponent(name, html) {
+/* 仅适用于不含js操作的简单组件 */
+function createComponent(name, html, script = null) {
   class myComponent extends HTMLElement {
     constructor() {
       super()
       let template = document.createElement('template')
       // 创建模板
       template.innerHTML = html
+      if (script) {
+        script()
+      }
       // 创建shadow root mode可以是open或者是closed,这定义了shadow root的内部实现是否可以被js访问及修改
       this._shadowRoot = this.attachShadow({ mode: 'closed' });
       this._shadowRoot.appendChild(template.content.cloneNode(true));
@@ -38,26 +42,24 @@ function createComponent(name, html) {
 
 
 
-
-
 // button
 let button = `
 <style>
-    button {
-      display: inline-block;
-      padding: 0.5rem 1rem;
-      background-color: rgb(0, 153, 255);
-      color: rgb(255, 255, 255);
-      border: 0;
-      border-radius: 0.3rem;
-      cursor: pointer;
-      transition: all .5 linear;
-    }
+button {
+  display: inline-block;
+  padding: 0.5rem 1rem;
+  background-color: rgb(0, 153, 255);
+  color: rgb(255, 255, 255);
+  border: 0;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  transition: all .5 linear;
+}
 
-    button:active {
-      transform: scale(0.9);
-      background-color: rgba(0, 153, 255, 0.8);
-    }
+button:active {
+  transform: scale(0.9);
+  background-color: rgba(0, 153, 255, 0.8);
+}
 </style>
 <button>
   <slot name="bicon"></slot>
@@ -77,10 +79,6 @@ input {
   padding: 0.5rem 1rem;
   border-radius: 0.3rem;
   font-size: 1rem;
-}
-
-input:placeholder-shown {
-  font-style: italic;
 }
 
 input:focus {
@@ -105,10 +103,6 @@ div {
   cursor: pointer;
 }
 
-a:link,
-a:visited {
-  color: rgb(70, 174, 247);
-}
 
 div:hover {
   color: rgba(70, 174, 247, 0.8);
@@ -118,10 +112,59 @@ div:hover {
 
 <div>
   <slot name="bicon"></slot>
-  <slot name="link"><a href="#">主要链接</a></slot>
+  <slot name="link">主要链接</slot>
   <slot name="aicon"></slot>
 </div>
 `
-
-
 createComponent('lk-link', link)
+
+// 开关
+let toggle = `
+<style>
+.LK-toggle {
+  vertical-align: middle;
+  appearance: none;
+  height: 1rem;
+  width: 2rem;
+  cursor: pointer;
+  background-color:#E8EAED ;
+  border-radius: 1rem;
+  position: relative;
+  padding-left: 1px;
+  outline: none;
+}
+
+.LK-toggle:checked {
+  background-color: #0099ff;
+  outline: none;
+}
+
+.LK-toggle::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 0.8rem;
+  height: 0.8rem;
+  border-radius: 50%;
+  top: 50%;
+  left:2px;
+  transform: translateY(-50%);
+  background-color: #fff;
+  transition: 0.5s;
+}
+
+.LK-toggle:checked::after {
+  outline: none;
+  transform-origin: center;
+  transform: translate(calc(1rem - 1px), -50%);
+}
+
+p{
+  display:inline;
+}
+</style>
+
+<input type='checkbox' class='LK-toggle'/>
+<slot name='label'>标签</slot>
+`
+createComponent('lk-toggle', toggle)
