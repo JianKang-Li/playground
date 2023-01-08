f96d3a1b557f47f46b67a6c809a93a02f53cfd15
李捡康 <3104653373@qq.com>
2023-01-08 14:08:20 +0800

    增加Vue2传送门组件

---
+++ js/Vue/components/MyPortal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div ref="div">
+    <slot></slot>
+  </div>
+</template>
+<script lang="js">
+export default {
+  props: ["to"],
+  mounted() {
+    const target = document.querySelector(this.to);
+    const operation = this.$refs.div;
+    target.appendChild(operation);
+  },
+};
+</script>
