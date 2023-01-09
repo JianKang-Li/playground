635c981353dc3de43d135a568b8d29d61f2bc448
李捡康 <3104653373@qq.com>
2023-01-09 20:57:41 +0800

    修改为双击右键防止误触

--- browser/双击关闭/index.js
+++ browser/双击关闭/index.js
@@ -1,7 +1,15 @@
 (function () {
   "use strict";
+  let time = null
   const body = document.querySelector("body")
-  body.addEventListener('dblclick', () => {
-    window.close()
+  body.addEventListener('contextmenu', () => {
+    if (time) {
+      if (Date.now() - time < 200) {
+        window.close()
+        time = null
+      }
+    } else {
+      time = Date.now()
+    }
   })
 })(); 
