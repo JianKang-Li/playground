e0a717a81e2f81ad8f8a11b9116526a8f2a2ea44
李捡康 <3104653373@qq.com>
2023-01-10 11:31:33 +0800

    回退

--- browser/双击关闭/index.js
+++ browser/双击关闭/index.js
@@ -1,15 +1,7 @@
 (function () {
   "use strict";
-  let time = null
   const body = document.querySelector("body")
-  body.addEventListener('contextmenu', () => {
-    if (time) {
-      if (Date.now() - time < 200) {
-        window.close()
-        time = null
-      }
-    } else {
-      time = Date.now()
-    }
+  body.addEventListener('dblclick', () => {
+    window.close()
   })
 })(); 
