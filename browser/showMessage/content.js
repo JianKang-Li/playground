4c851779d8a0a1cba71e76445f39340b98a77221
ljk <72432476+JianKang-Li@users.noreply.github.com>
2023-03-02 18:55:01 +0800

    Create content.js

--- browser/showMessage/content.js
+++ browser/showMessage/content.js
@@ -4,23 +4,24 @@ div.style = 'display:none;position:fixed;top:0;right:0;width:250px;height:250px;
 div.innerHTML = `<div id='showMessage' style="padding:10px;">没有选择元素</div><button id='cancelBtn' style='border:none;background-color:#eee;padding:5px 20px;border-radius:5px;margin-top:10px;'>隐藏</button>`
 document.body.appendChild(div)
 const pop = document.querySelector('#stylepop')
-document.body.addEventListener('click', (e) => {
-  const ele = e.target
-  const styles = window.getComputedStyle(ele)
-  const div = document.querySelector('#showMessage')
-  div.innerHTML = `
-  <p style='line-height:1;margin:0;padding:0;'>目标：${e.target.nodeName}</p>
-  <p style='line-height:1;margin:0;padding:0;'>类：${e.target.classList}</p>
-  <p style='line-height:1;margin:0;padding:0;'>宽度：${styles.width}</p>
-  <p style='line-height:1;margin:0;padding:0;'>高度：${styles.height}</p>
-  <p style='line-height:1;margin:0;padding:0;display:inline;'>颜色：${styles.color}</p><div style='display:inline-block;width:10px;height:10px;background-color:${styles.color}'></div>
-  <p style='line-height:1;margin:0;padding:0;'>背景：${styles.background}</p>
-  <p style='line-height:1;margin:0;padding:0;'>margin：${styles.margin}</p>
-  <p style='line-height:1;margin:0;padding:0;' >padding：${styles.padding}</p>
-  <p style='line-height:1;margin:0;padding:0;'>字体：${styles.fontFamily}</p>
-  <p style='line-height:1;margin:0;padding:0;'>字号：${styles.fontSize}</p>
-  `
-})
+function update(e){
+    const ele = e.target
+    const styles = window.getComputedStyle(ele)
+    const div = document.querySelector('#showMessage')
+    div.innerHTML = `
+    <p style='line-height:1;margin:0;padding:0;'>目标：${e.target.nodeName}</p>
+    <p style='line-height:1;margin:0;padding:0;'>类：${e.target.classList}</p>
+    <p style='line-height:1;margin:0;padding:0;'>宽度：${styles.width}</p>
+    <p style='line-height:1;margin:0;padding:0;'>高度：${styles.height}</p>
+    <p style='line-height:1;margin:0;padding:0;display:inline;'>颜色：${styles.color}</p><div style='display:inline-block;width:10px;height:10px;background-color:${styles.color}'></div>
+    <p style='line-height:1;margin:0;padding:0;'>背景：${styles.background}</p>
+    <p style='line-height:1;margin:0;padding:0;'>margin：${styles.margin}</p>
+    <p style='line-height:1;margin:0;padding:0;' >padding：${styles.padding}</p>
+    <p style='line-height:1;margin:0;padding:0;'>字体：${styles.fontFamily}</p>
+    <p style='line-height:1;margin:0;padding:0;'>字号：${styles.fontSize}</p>
+    `
+}
+document.body.addEventListener('click',update)
 
 if (pop.style.display === 'none') {
   pop.style.display = 'block'
@@ -30,10 +31,12 @@ const cancel = document.querySelector('#cancelBtn')
 cancel.onclick = (e) => {
   e.stopPropagation()
   pop.style.display = 'none'
+  document.body.removeEventListener('click',update)
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if(request==='open'){
     pop.style.display='block'
+    document.body.addEventListener('click',update)
   }
 });
