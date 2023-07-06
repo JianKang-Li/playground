66b2075b6b6d771f37a5ed0e2dafc4cfb2989929
李捡康 <3104653373@qq.com>
2023-07-06 22:33:20 +0800

    leetcode去导航栏

---
+++ js/Tampermonkey/leetcode.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         leetcode clean
+// @namespace    http://tampermonkey.net/
+// @version      0.0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://leetcode.cn/problems/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leetcode.cn
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  window.onload = function () {
+    const button = document.createElement('button')
+    const body = document.body
+    const container = document.querySelector('.flex.h-full.flex-row.gap-8') || document.querySelector('.css-1lexzqe-TabHeaderContainer.e16udao2')
+    const list = ['.css-1k63xn3-HeaderCn.e1t1fzqp0', "#__next > div > div > div > nav"]
+    let flag = true
+    button.innerText = '隐藏'
+    button.setAttribute('style', `
+    top: 10px;
+    left: 20px;
+    z-index: 999;
+    background: #eee;
+    padding: 5px 10px;
+    border-radius: 5px;
+    `)
+
+    button.addEventListener('click', () => {
+      for (const item of list) {
+        const ele = document.querySelector(item)
+
+        if (flag && ele) {
+          ele.style.display = 'none'
+          flag = false
+        } else if (ele) {
+          ele.style.display = 'block'
+          flag = true
+        }
+      }
+    })
+
+    if (container) {
+      container.appendChild(button)
+    } else {
+      body.appendChild(button)
+    }
+  }
+})();
