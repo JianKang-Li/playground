19553a60690826eef2dea96d4033b0975b3b40e8
李捡康 <3104653373@qq.com>
2023-07-08 01:39:41 +0800

    仿VSCODE

--- js/Tampermonkey/leetcode.js
+++ js/Tampermonkey/leetcode.js
@@ -12,39 +12,78 @@
 (function () {
   'use strict';
   window.onload = function () {
-    const button = document.createElement('button')
     const body = document.body
-    const container = document.querySelector('.flex.h-full.flex-row.gap-8') || document.querySelector('.css-1lexzqe-TabHeaderContainer.e16udao2')
-    const list = ['.css-1k63xn3-HeaderCn.e1t1fzqp0', "#__next > div > div > div > nav"]
+    // 自定义样式类兼容旧版
+    const style = document.createElement('style')
+    style.innerHTML = `
+    .jk-flex-col {
+      display: flex;
+      flex-direction: column;
+      width: fit-content;
+    }
+
+    .jk-flex-row {
+      display: flex;
+      flex-direction: row;
+    }
+    `
+
+    body.appendChild(style)
+    // 讨论栏
+    const container = document.querySelector('#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div:nth-child(1) > div > div') || document.querySelector('.css-1lexzqe-TabHeaderContainer.e16udao2') || document.querySelector('#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div > div:nth-child(1)')
+    // 左侧题目栏
+    const leftContain = document.querySelector('#qd-content > div.h-full.flex-col.ssg__qd-splitter-primary-w > div > div > div') || document.querySelector('.css-11zaw7c-TabView')
+    // 旧版tab容器
+    const div = document.querySelector('#question-detail-main-tabs > div.css-1hbqv5m-TabViewHeader.e16udao1')
+    // 隐藏元素列表
+    const list = ['.css-1k63xn3-HeaderCn.e1t1fzqp0', "#__next > div > div > div > nav", '#lc-home > div > div.main__1pRE > div.css-wfkj6v-Content.e1aolq224 > div > div.css-1gcn2k5-RightContainer.e1aolq221 > div > div.css-w8jsc9-EditorContainer.ejldciv1 > div > div.container__39KX > div.second-section-container__2cAh > div.css-ontzvu-MockInterviewContainer.e3jm4na1 > div.css-1mu9tzo-WideContainer.e3jm4na3']
+    // 隐藏标志
     let flag = true
-    button.innerText = '隐藏'
+
+    // 自定义按钮
+    const button = document.createElement('button')
+    button.innerText = 'vscode'
     button.setAttribute('style', `
-    top: 10px;
-    left: 20px;
     z-index: 999;
-    background: #eee;
-    padding: 5px 10px;
+    padding: 5px 5px;
     border-radius: 5px;
+    background: #f7f7f7;
+    border: none;
     `)
 
     button.addEventListener('click', () => {
-      for (const item of list) {
-        const ele = document.querySelector(item)
-
-        if (flag && ele) {
-          ele.style.display = 'none'
-          flag = false
-        } else if (ele) {
-          ele.style.display = 'block'
-          flag = true
+      if (flag) {
+        for (const item of list) {
+          const ele = document.querySelector(item)
+          ele && (ele.style.display = 'none')
         }
+        container && container.classList.add('jk-flex-col')
+        container && container.classList.remove('jk-flex-row')
+        leftContain && leftContain.classList.remove('jk-flex-col')
+        leftContain && leftContain.classList.add('jk-flex-row')
+        div && (div.style.width = 'fit-content')
+        flag = false
+      } else {
+        for (const item of list) {
+          const ele = document.querySelector(item)
+          ele && (ele.style.display = 'block')
+        }
+        container && container.classList.remove('jk-flex-col')
+        container && container.classList.add('jk-flex-row')
+        leftContain && leftContain.classList.add('jk-flex-col')
+        leftContain && leftContain.classList.remove('jk-flex-row')
+        div && (div.style.width = '100%')
+        flag = true
       }
     })
 
     if (container) {
       container.appendChild(button)
     } else {
+      // 兜底
       button.style.position = 'fixed'
+      button.style.top = '10px'
+      button.style.left = '10px'
       body.appendChild(button)
     }
   }
