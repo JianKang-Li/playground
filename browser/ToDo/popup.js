287d45677cb9bc0628bc2ac9521532c670192887
李捡康 <3104653373@qq.com>
2023-11-15 22:05:38 +0800

    实现主要功能

--- browser/ToDo/popup.js
+++ browser/ToDo/popup.js
@@ -1,54 +1,90 @@
-class Local {
-  constructor() {
-    this.local = localStorage
-  }
-
-  add(key, value) {
-    return this.local.setItem(key, value)
-  }
-
-  remove(key) {
-    return this.local.removeItem(key)
-  }
-
-  clear() {
-    return this.local.clear()
-  }
-
-  get(key) {
-    return this.local.getItem(key)
-  }
-}
-
 const input = document.querySelector('#todo')
+const addBtn = document.querySelector('#addBtn')
 const container = document.querySelector('#todos')
-const local = new Local()
+const clean = document.querySelector('#clean')
+const local = chrome.storage.local
 
 function render() {
-  const todos = JSON.parse(local.get('todo'))
-  container.innerHTML = ''
-  if (todos) {
-    const ul = document.createElement('ul')
-    Object.keys(todos).forEach(key => {
-      const li = document.createElement('li')
-      li.innerHTML = `<input type="checkbox" value="${todos[key]}" /><span>${key}</span>`
-      ul.appendChild(li)
-    })
-    container.appendChild(ul)
-  }
+  local.get('todo', res => {
+    const todos = res.todo
+    container.innerHTML = ''
+
+    if (todos) {
+      const ul = document.createElement('ul')
+
+      ul.setAttribute('class', 'lists')
+      Object.keys(todos).forEach(key => {
+        const li = document.createElement('li')
+        li.innerHTML = `<input type="checkbox" data-key="${key}" id="li_${key}"/><span>${key}</span>&nbsp;&nbsp;&nbsp;&nbsp;<i class="delete" data-key="${key}"/>`
+        ul.appendChild(li)
+      })
+      container.appendChild(ul)
+      Object.keys(todos).forEach(key => {
+        const li = document.querySelector(`#li_${key}`)
+
+        if (todos[key]) {
+          li.checked = true
+        } else {
+          li.checked = false
+        }
+      })
+      addEvent()
+    }
+  })
+}
+
+function addEvent() {
+  const ul = document.querySelector('.lists')
+
+  ul.addEventListener('click', (e) => {
+    if (!e.target.classList.value.includes('delete')) {
+      const key = e.target.dataset['key']
+      const value = e.target.checked
+
+      local.get('todo', res => {
+        let todos = {}
+        Object.keys(res.todo).forEach(item => {
+          todos[item] = res.todo[item]
+        })
+
+        todos[key] = value
+        local.set({ 'todo': todos })
+      })
+    } else {
+      const key = e.target.dataset['key']
+
+      local.get('todo', res => {
+        let todos = res.todo
+
+        delete todos[key]
+        local.set({ 'todo': todos }, () => {
+          render()
+        })
+      })
+    }
+  })
 }
 
 function add() {
-  const key = input.value
-  let todos = JSON.parse(local.get('todo'))
-  if (!todos) {
-    todos = {}
-  }
-  todos[key] = false
-  local.add('todo', JSON.stringify(todos))
-  render()
+  local.get('todo', res => {
+    const key = input.value
+    let todos = res.todo
+
+    if (!todos) {
+      todos = {}
+    }
+    todos[key] = false
+    local.set({ 'todo': todos }, () => {
+      render()
+    })
+  })
 }
 
+addBtn.addEventListener('click', add)
+clean.addEventListener('click', () => {
+  local.clear()
+  render()
+})
 window.addEventListener('load', () => {
   render()
 })
