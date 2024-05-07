5fa06727f4da15caa74a5b514e0f35992709f7e2
Jiankang Li <lijiankang@scutech.com>
2024-05-07 11:14:08 +0800

    修改notify

--- js/notify.js
+++ js/notify.js
@@ -1,20 +1,30 @@
-function notify({ message = 'Tip', position = 'right' }) {
+function notify({ message = 'Tip', position = 'right', delay = 5 }) {
+  function objToStr (obj) {
+    let text = ''
+    Object.keys(obj).forEach(key => {
+      text += `${key}: ${obj[key]};`
+    })
+
+    return text
+  }
+
   const body = document.body
   const span = document.createElement('span')
-  span.style = `position: fixed;
-  bottom: 10px;
-  right: 10px;
-  background-color: #eee;
-  padding: 4.8px 10px;
-  border-radius: 4.8px;
-  z-index:9999;
-  box-shadow': '0 4px 12px 0 rgba(0, 0, 0, 0.05);
-  `
+  span.style = objToStr({
+    position: 'absolute',
+    bottom: '10px',
+    right: '10px',
+    'background-color': '#eee',
+    padding: '4.8px 10px',
+    'border-radius': '4.8px',
+    'z-index':9999,
+    'box-shadow': '0 4px 12px 0 rgba(0, 0, 0, 0.05)',
+  })
   span.textContent = message
   body.appendChild(span)
   setTimeout(() => {
     span.remove()
-  }, 1000)
+  }, delay * 1000)
 }
 
-export default notify
+window.notify = notify
